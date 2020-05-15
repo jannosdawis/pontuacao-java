efbe8ddf219d306f458ed2b1ce520924a16fb51f
jannosdawis <jdawis@outlook.com>
2020-05-15 19:36:27 -0300

    Leitura dos outros dados

--- src/aplicacao/programa.java
+++ src/aplicacao/programa.java
@@ -5,8 +5,9 @@ public class programa {
 	public static void main(String[] args) {
 		Locale.setDefault(Locale.US);
 		Scanner sc = new Scanner(System.in);
-		int compras;
+		int compras, atraso;
 		double ticket;
+		char tipocompra;
 		
 	// CABEÇALHO
 		System.out.println("SISTEMA DE PERFIL DE CLIENTE");
@@ -20,6 +21,13 @@ public class programa {
 		System.out.print("Qual o ticket médio? ");
 		ticket = sc.nextDouble();
 		System.out.println("");
+		
+	// LEITURA DOS OUTROS DADOS
+		System.out.print("Quantas vezes o cliente atrasou o pagamento? ");
+		atraso = sc.nextInt();
+		System.out.print("A maioria das compras foi em dinheiro, cartão, ou boleto (D/C/B)? ");
+		tipocompra = sc.next().charAt(0);
+		
 		
 		
 		sc.close();
