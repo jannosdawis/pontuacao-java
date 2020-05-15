b73fab3532098b09e85081d866575b5d3c2ee4d7
jannosdawis <jdawis@outlook.com>
2020-05-15 19:28:52 -0300

    Leitura do volume

--- src/aplicacao/programa.java
+++ src/aplicacao/programa.java
@@ -1,12 +1,27 @@
 package aplicacao;
 import java.util.Scanner;
+import java.util.Locale;
 public class programa {
 	public static void main(String[] args) {
+		Locale.setDefault(Locale.US);
 		Scanner sc = new Scanner(System.in);
+		int compras;
+		double ticket;
+		
+	// CABEÇALHO
 		System.out.println("SISTEMA DE PERFIL DE CLIENTE");
 		System.out.println("--------------------------------");
 		System.out.println("INFORME OS DADOS DO ÚLTIMO ANO");
 		System.out.println("");
+	
+	// LEITURA DO VOLUME
+		System.out.print("Quantas compras o cliente fez no último ano? ");
+		compras = sc.nextInt();
+		System.out.print("Qual o ticket médio? ");
+		ticket = sc.nextDouble();
+		System.out.println("");
+		
+		
 		sc.close();
 	}
 
